19d0d84d04e00a163a50e16dda59fb83f1ccbcb3
yam-k <yam.k.o212@gmail.com>
2024-09-24 00:57:19 +0900

    switch-windowを追加

--- emacs/init.el
+++ emacs/init.el
@@ -492,6 +492,27 @@ ARGSは、[KEY FUNCTION]..."
   '(("\\`C-c C-o\\'" . nil) . (nil . "outline-map"))
   )
 
+;;;; switch-window
+(package-install 'switch-window)
+(setq switch-window-shortcut-style 'qwerty
+      switch-window-input-style 'minibuffer
+      switch-window-threshold 2
+      )
+(setkey global-map
+  "C-x o" #'switch-window
+  "C-x 1" #'switch-window-then-maximize
+  "C-x 2" #'switch-window-then-split-below
+  "C-x 3" #'switch-window-then-split-right
+  "C-x 0" #'switch-window-then-delete
+  "C-x 4 d" #'switch-window-then-dired
+  "C-x 4 f" #'switch-window-then-find-file
+  "C-x 4 m" #'switch-window-then-compose-mail
+  "C-x 4 r" #'switch-window-then-find-file-read-only
+  "C-x 4 C-f" #'switch-window-then-find-file
+  "C-x 4 C-o" #'switch-window-then-display-buffer
+  "C-x 4 0" #'switch-window-then-kill-buffer
+  )
+
 ;;;; blackout --------------------------------------------------------
 (package-install 'blackout) ;モードラインのモード表示を消す
 
